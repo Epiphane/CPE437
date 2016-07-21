8edff20fd84600f43b7327f178542add3a4e90b5
Enoch <ectsui@users.noreply.github.com>
2016-07-21 14:27:11 -0700

    Enrs endpoint

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ app.use('/Ssns', require('./Routes/Account/Ssns'));
 app.use('/Chls', require('./Routes/Challenge/Chls'));
 app.use('/Atts', require('./Routes/Challenge/Atts'));
 app.use('/Crss', require('./Routes/Course/Crss'));
+app.use('/Enrs', require('./Routes/Course/Enrs'));
 
 app.put('/Time', function(req, res) {
    req.validator.checkAdmin()
@@ -137,7 +138,7 @@ app.use(function(err, req, res, next) {
    res.status(500).send('error', {error: err});
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.NODE_PORT || 3000, 
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.NODE_PORT || 3000,
            process.env.OPENSHIFT_NODEJS_IP   || process.env.NODE_IP   || 'localhost',
 function () {
    console.log('App Listening on port 3000');
